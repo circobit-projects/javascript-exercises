6b59e04fff8e0836ed704bf0e2ca6a1ef600665a
Cristian Cevasco <cristian.cevasco@gmail.com>
2026-06-26 05:43:33 +0200

    06_repeatString: add logic for repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+	if (num < 0) {
+		return "ERROR";
+	};
 
+	let stringToReturn = "";
+	while (num > 0) {
+		stringToReturn = stringToReturn + string;
+		num--;
+	};
+
+	return stringToReturn;
 };
 
 // Do not edit below this line
