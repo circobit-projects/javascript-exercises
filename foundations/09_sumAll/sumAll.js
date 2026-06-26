f025f23b6cf3d2b6fb5b1db5f1067c7e2cfe6824
Cristian Cevasco <cristian.cevasco@gmail.com>
2026-06-26 08:41:33 +0200

    09_sumAll: add logic for sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,38 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+	let lowestNum = 0;
+	let highestNum = 0;
 
+	// Check if the arguments are not a number
+	if (isNaN(int1) && isNaN(int2)) {
+		return "ERROR";
+	// Check if numbers are negative
+	} else if (int1 < 0 || int2 < 0) {
+		return "ERROR";
+	// Check if numbers are not integers
+	} else if (!Number.isInteger(int1) || !Number.isInteger(int2)) {
+		return "ERROR";
+	};
+
+	if (int1 < int2) {
+		lowestNum = int1;
+		highestNum = int2;
+	} else {
+		lowestNum = int2;
+		highestNum = int1;
+	};
+
+	let integers = [];
+	let sumOfEveryInteger = 0;
+
+	for (let i = lowestNum; i <= highestNum; i++) {
+		integers.push(i);
+	};
+
+	for (let i = 0; i < integers.length; i++) {
+		sumOfEveryInteger = sumOfEveryInteger + integers[i];
+	};
+
+	return sumOfEveryInteger;
 };
 
 // Do not edit below this line
