de1c37bf3d6a733a6ca226b1c78dce3f07dc9c7c
Cristian Cevasco <cristian.cevasco@gmail.com>
2026-06-26 09:09:44 +0200

    11_tempConversion: add logic for convertToCelsius and convertToFahrenheit

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(numToConvert) {
+	let numInCelsius = (numToConvert - 32) / 1.8;
+	let roundUpTemp = Math.round(numInCelsius * 10) / 10;
+	return roundUpTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(numToConvert) {
+	let numInFahrenheit = numToConvert * 1.8 + 32;
+	let roundUpTemp = Math.round(numInFahrenheit * 10) / 10;
+	return roundUpTemp;
 };
 
 // Do not edit below this line
