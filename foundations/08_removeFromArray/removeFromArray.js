4110ffe95816de55ec114e14c840083603f23da1
Cristian Cevasco <cristian.cevasco@gmail.com>
2026-06-26 07:28:44 +0200

    08_removeFromArray: add logic for removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToModify, ...args) {
+	let modifiedArray = arrayToModify;
+
+	for (const arg of args) {
+		modifiedArray = modifiedArray.filter((item) => item !== arg);
+	};
+	return modifiedArray;
 };
 
 // Do not edit below this line
